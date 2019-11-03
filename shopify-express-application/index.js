8517ecdec5e9f063251080ccc7b7fb105fb7a450
nihalkaradan <nihalkaradan@gmail.com>
2019-11-03 23:06:55 +0530

    basic app done

--- shopify-express-application/index.js
+++ shopify-express-application/index.js
@@ -35,7 +35,6 @@ app.get('/shopify', (req, res) => {
 
 //after app is installed this endpoint gets hit 
 app.get('/shopify/callback', (req, res) => {
-	
   const { shop, hmac, code, state } = req.query;
   const stateCookie = cookie.parse(req.headers.cookie).state;
 
@@ -44,12 +43,60 @@ app.get('/shopify/callback', (req, res) => {
   }
 
   if (shop && hmac && code) {
-    res.status(200).send('Callback route');
+    // DONE: Validate request is from Shopify
+    const map = Object.assign({}, req.query);
+    delete map['signature'];
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const providedHmac = Buffer.from(hmac, 'utf-8');
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+        'utf-8'
+      );
+    let hashEquals = false;
 
-    // TODO
-    // Validate request is from Shopify
-    // Exchange temporary code for a permanent access token
-      // Use access token to make API call to 'shop' endpoint
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    } catch (e) {
+      hashEquals = false;
+    };
+
+    if (!hashEquals) {
+      return res.status(400).send('HMAC validation failed');
+    }
+
+    // DONE: Exchange temporary code for a permanent access token
+    const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+    const accessTokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code,
+    };
+
+    request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+    .then((accessTokenResponse) => {
+      const accessToken = accessTokenResponse.access_token;
+      // DONE: Use access token to make API call to 'shop' endpoint
+      const shopRequestUrl = 'https://' + shop + '/admin/api/2019-10/shop.json';
+      const shopRequestHeaders = {
+        'X-Shopify-Access-Token': accessToken,
+      };
+
+      request.get(shopRequestUrl, { headers: shopRequestHeaders })
+      .then((shopResponse) => {
+        res.status(200).end(shopResponse);
+      })
+      .catch((error) => {
+        res.status(error.statusCode).send(error.error.error_description);
+      });
+    })
+    .catch((error) => {
+      res.status(error.statusCode).send(error.error.error_description);
+    });
+
   } else {
     res.status(400).send('Required parameters missing');
   }
