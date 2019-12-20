e414c0d05c6903df7333f84020685c2e3275445d
Salman Thaikat <salmanthaikkat@gmail.com>
2019-12-20 22:47:40 +0530

    Added authentication and connected to the shop

---
+++ routes/report-routes.js
@@ -0,0 +1,97 @@
+const router = require('express').Router();
+const nonce = require('nonce')();
+const cookie = require('cookie');
+
+const querystring = require('querystring');
+const request = require('request-promise');
+const crypto = require('crypto');
+const Shopify = require('shopify-api-node');
+
+const apiKey = process.env.SHOPIFY_API_KEY;
+const apiSecret = process.env.SHOPIFY_API_SECRET;
+const scopes = 'read_products';
+const forwardingAddress = "https://4dabfc09.ngrok.io";
+
+
+router.get('/shopify', (req, res) => {
+    const shop = req.query.shop;
+    if (shop) {
+        const state = nonce();
+        const redirectUri = forwardingAddress + '/shopify/callback';
+        const installUrl = 'https://' + shop +
+            '/admin/oauth/authorize?client_id=' + apiKey +
+            '&scope=' + scopes +
+            '&state=' + state +
+            '&redirect_uri=' + redirectUri;
+
+        res.cookie('state', state);
+        res.redirect(installUrl);
+    } else {
+        return res.status(400).send('Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request');
+    }
+});
+
+router.get('/shopify/callback', (req, res) => {
+    const { shop, hmac, code, state } = req.query;
+    const stateCookie = cookie.parse(req.headers.cookie).state;
+
+    if (state !== stateCookie) {
+        return res.status(403).send('Request origin cannot be verified');
+    }
+
+    if (shop && hmac && code) {
+        const map = Object.assign({}, req.query);
+        delete map['signature'];
+        delete map['hmac'];
+        const message = querystring.stringify(map);
+        const providedHmac = Buffer.from(hmac, 'utf-8');
+        const generatedHash = Buffer.from(
+            crypto
+                .createHmac('sha256', apiSecret)
+                .update(message)
+                .digest('hex'),
+            'utf-8'
+        );
+        let hashEquals = false;
+
+        try {
+            hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+        } catch (e) {
+            hashEquals = false;
+        };
+
+        if (!hashEquals) {
+            return res.status(400).send('HMAC validation failed');
+        }
+
+        const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+        const accessTokenPayload = {
+            client_id: apiKey,
+            client_secret: apiSecret,
+            code,
+        };
+
+        request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+            .then((accessTokenResponse) => {
+                const accessToken = accessTokenResponse.access_token;
+
+                const shopify = new Shopify({
+                    shopName: 'tool-x1.myshopify.com',
+                    accessToken: accessToken
+                });
+
+                console.log(shopify);
+
+                res.status(200).send("Got an access token, let's do something with it");
+            })
+            .catch((error) => {
+                res.status(error.statusCode).send(error);
+            });
+
+    } else {
+        res.status(400).send('Required parameters missing');
+    }
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const reportRoutes = require('./routes/report-routes');
+
+app.use('/', reportRoutes);
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
